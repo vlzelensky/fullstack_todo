96c41b1090d421ef277c8b5dd75ea0736ece26bd
vlzelensky <vlzelensky.exceedteam@gmail.com>
2020-12-16 22:45:51 +0300

    saveUserData

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom';
 import LoginComponent from './components/LoginComponent';
-import RegisterComponent from './components/RegisterComponent';
 
 function App() {
   return (
       <BrowserRouter>
-        <Route path="/login" component={LoginComponent}/>
+        <Switch>
+          <Route path="/login" component={LoginComponent}/>
+          <Redirect to="/login"/>
+        </Switch>
       </BrowserRouter>
   );
 }
--- src/components/RegisterComponent/index.js
+++ src/components/RegisterComponent/index.js
@@ -1,39 +1,116 @@
-import React from 'react';
+import React from "react";
+import axios from "axios";
 class RegisterComponent extends React.Component {
-
-    render() {
-        return (
-        <div className='main-box'>
-            <h1>registration</h1>
-            <div className='first-name'>
-                <span>First name</span>
-                <input className='input' placeholder='First name'></input>
-            </div>
-            <div className='second-name'>
-                <span>Second name</span>
-                <input className='input' placeholder='Second name'></input>
-            </div>
-            <div className='email'>
-                <span>Email</span>
-                <input className='input' placeholder='Email'></input>
-            </div>
-            <div className='password'>
-                <span>Password</span>
-                <input className='input' placeholder='Password' type='password'></input>
-            </div>
-            <div className='password'>
-                <span>Repeat Password</span>
-                <input className='input' placeholder='Repeat password' type='password'></input>
-            </div>
-            <div className='btns'>
-                <button id="123123" className='btn1' onClick={this.props.changeStatus}>Back</button>
-                <button className='btn1'>Create account</button>
-            </div>
-        </div>
-        );
+  state = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    repeatPassword: "",
+  };
+  saveUserData = async () => {
+    if (Object.values(this.state).some((value) => value === "")){
+        return;
     }
-  
-}
+    if (this.state.password === this.state.repeatPassword) {
+      try {
+        await axios.post("/api/user", {
+          firstName: this.state.firstName,
+          lastName: this.state.lastName,
+          email: this.state.email,
+          password: this.state.password,
+        });
+        this.setState({
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: "",
+            repeatPassword: "",
+          });
+      } catch (e) {
+        console.warn(e);
+      }
+    } else {
+      alert("passwords don't match");
+    }
+  };
 
+  render() {
+    return (
+      <div className="main-box">
+        <h1>registration</h1>
+        <div className="first-name">
+          <span>First name</span>
+          <input
+            className="input"
+            onChange={(event) =>
+              this.setState({ firstName: event.target.value.trim() })
+            }
+            value={this.state.firstName}
+            placeholder="First name"
+          ></input>
+        </div>
+        <div className="second-name">
+          <span>Second name</span>
+          <input
+            className="input"
+            onChange={(event) =>
+              this.setState({ lastName: event.target.value.trim() })
+            }
+            value={this.state.lastName}
+            placeholder="Second name"
+          ></input>
+        </div>
+        <div className="email">
+          <span>Email</span>
+          <input
+            className="input"
+            onChange={(event) =>
+              this.setState({ email: event.target.value.trim() })
+            }
+            value={this.state.email}
+            placeholder="Email"
+          ></input>
+        </div>
+        <div className="password">
+          <span>Password</span>
+          <input
+            className="input"
+            onChange={(event) =>
+              this.setState({ password: event.target.value.trim() })
+            }
+            placeholder="Password"
+            value={this.state.password}
+            type="password"
+          ></input>
+        </div>
+        <div className="password">
+          <span>Repeat Password</span>
+          <input
+            className="input"
+            onChange={(event) =>
+              this.setState({ repeatPassword: event.target.value.trim() })
+            }
+            placeholder="Repeat password"
+            value={this.state.repeatPassword}
+            type="password"
+          ></input>
+        </div>
+        <div className="btns">
+          <button
+            id="123123"
+            className="btn1"
+            onClick={this.props.changeStatus}
+          >
+            Back
+          </button>
+          <button className="btn1" onClick={this.saveUserData}>
+            Create account
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default RegisterComponent;
